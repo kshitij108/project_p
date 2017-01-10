40de5ec3d4ef56d4b6e7666a3b259abfbdfb2615
Kshitij Bhatt <kshitij108@gmail.com>
2017-01-10 11:53:09 -0500

    updated for python 2.7

--- Final_submited.py
+++ Final_submited.py
@@ -1,10 +1,14 @@
 #Here is my code.
 # importing file.
-from decruft import Document
+from readability.readability import Document
 import urllib2
 # Get Users URL
-URL=raw_input("ENTER THE URL: ") 
+URL= "http://arstechnica.com/science/2017/01/texas-slams-fda-with-lawsuit-for-holding-up-imported-execution-drugs/"
+#req = urllib2.Request(URL)
 # putting it in to object.
+print ("url is - " + URL)
+#URL = URL.strip('\'"')
+#print ("new url is - " + URL)
 fURL = urllib2.urlopen(URL)
 #Making Html file 
 htmlName="decrufted.html"
@@ -16,7 +20,7 @@ htmlThing = open(htmlName,'w')
 #making srting object.
 strHtmlStuff = Document(fURL.read()).summary()
 #Writing stuff from string object.
-htmlThing.write(strHtmlStuff)
+htmlThing.write(strHtmlStuff.encode('utf8') + '\n')
 htmlThing.close
 print "The file name is: " + htmlName
 # there u go with html file.
